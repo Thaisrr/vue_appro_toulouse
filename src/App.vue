6b0859ddfbb48308f63ba4d70d5a2fdda4812dbb
Thaïs Révillon <thais.r@live.fr>
2020-11-13 16:49:02 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu :title="message"></Menu>
+    <router-link :to="{name: 'home', params: {param_title: app_title } }">Home</router-link>
+    <router-view></router-view>
+    <TestProps :name=" 'Noah' " :age="345" :title="app_title" :my-user="user"></TestProps>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from "@/components/Menu";
+import TestProps from "@/components/TestProps";
 
 export default {
   name: 'App',
+  data: function () {
+    return {
+      message : 'My First App',
+      number_users: 345,
+      app_title: 'Hello World',
+      user: {user_name: 'Toto', user_mail: 'toto@gmail.com'},
+      isConnected: false
+    }
+  },
+  computed: {
+    truc: function () {
+     if(this.isConnected)
+        return 'bidule'
+     else
+       return  'autre chose'
+    }
+  },
   components: {
-    HelloWorld
+    TestProps,
+    Menu,
   }
 }
+
+
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router";
+import Home from "@/components/Home";
+import HelloWorld from "@/components/HelloWorld";
+import Todos from "@/components/Todos";
+import AddTodo from "@/components/AddTodo";
+
+// On définit une route pour chaque composant :
+// ( Attention à bien importer les composants )
+const routes = [
+  {path: '/home', name: 'home', component: Home},
+  {path: '/hello', name: 'helloworld', component: HelloWorld},
+  {path: '/todos', name: 'todos', component: Todos},
+  {path: '/add-todo', name: 'add', component: AddTodo}
+];
+
+// On créé l'instance de Vue Router
+const router = new VueRouter({
+  routes
+})
+
+
+Vue.use(VueRouter); // On dit à Vue qu'on utilise Vue Router
 
 Vue.config.productionTip = false
 
+
 new Vue({
+  router, // on ajoute vue dans l'instance de Vue
   render: h => h(App),
 }).$mount('#app')
